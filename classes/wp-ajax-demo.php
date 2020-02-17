539da57a1bb40382d6b3d6cd3d471c0d8895d7ff
petercz1 <email.peter.carroll@gmail.com>
2020-02-17 14:54:58 +0800

    You will outgrow your usefulness.

--- classes/wp-ajax-demo.php
+++ classes/wp-ajax-demo.php
@@ -14,8 +14,8 @@ class WP_Ajax_Demo
         $wp_ajax_menuitem = new WP_Ajax_Menuitem();
         add_action('admin_menu', array($wp_ajax_menuitem, 'init'));			// loads menu item
 
-        $wp_ajax_js = new WP_Ajax_Js();
-        add_action('admin_footer', array($wp_ajax_js, 'init'));				// loads ajax js
+        // $wp_ajax_js = new WP_Ajax_Js();
+        // add_action('admin_footer', array($wp_ajax_js, 'init'));				// loads ajax js
 
         // this is the weird one. The WordPress ajax handler needs 'wp_ajax_' in front of the requested function...
         $wp_ajax_handler = new WP_Ajax_Db_Handler();
