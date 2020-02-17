28278fe4229f0e893d6c2d3d1cd0d85935018313
petercz1 <email.peter.carroll@gmail.com>
2020-02-17 14:57:52 +0800

    The fashion wears out more apparel than the man. 		-- William Shakespeare, "Much Ado About Nothing"

--- classes/wp-ajax-loadjs.php
+++ classes/wp-ajax-loadjs.php
@@ -6,6 +6,6 @@ class WP_Ajax_Loadjs
     public function init()
     {
         error_log('loading js...');
-        wp_enqueue_style('WP-ajax-js', plugins_url('js/wp-ajax.js', __FILE__));
+        wp_enqueue_script('WP-ajax-js', plugins_url('js/wp-ajax.js', __FILE__));
     }
 }
