8b420c5277c8a80506be8b0f93d87ec9280589e0
petercz1 <email.peter.carroll@gmail.com>
2020-02-17 14:56:07 +0800

    What good is an obscenity trial except to popularize literature? 		-- Nero Wolfe, "The League of Frightened Men"

--- classes/wp-ajax-loadjs.php
+++ classes/wp-ajax-loadjs.php
@@ -6,6 +6,6 @@ class WP_Ajax_Loadjs
     public function init()
     {
         error_log('loading js...');
-        wp_enqueue_style('WP-ajax-js', plugins_url('js/WP-ajax.css', __FILE__));
+        wp_enqueue_style('WP-ajax-js', plugins_url('js/wp-ajax.css', __FILE__));
     }
 }
